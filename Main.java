3fe1c2346742bd0233eaad56b88b482b1082a196
NathanGR17 <166703493+NathanGR17@users.noreply.github.com>
2026-03-30 20:11:06 +0000

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         Warehouse bcnWarehouse = new Warehouse();
-        Product sneakers = new Product("JD Shoes", 5);
+        Product sneakers = new Product("Shoes", 5);
 
         for (int i = 1; i <= 10; i++) {
             new UserThread(bcnWarehouse, sneakers, "User_" + i).start(); // Each user thread will attempt to purchase the sneakers
